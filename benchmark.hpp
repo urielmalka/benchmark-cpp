bd05c03b55b7900951d4e1884f0bacb7b3e28b73
urielmalka <urielmalka1@gmail.com>
2025-08-26 23:02:24 +0300

    init benchmark.cpp file

---
+++ benchmark.hpp
@@ -0,0 +1,30 @@
+#ifndef BENCMARK_HPP
+#define BENCMARK_HPP
+
+#include <iostream>
+#include <ctime>
+
+clock_t start;
+int task_id = 0;
+
+void initBenchmark()
+{
+    start = clock();
+}
+
+void startArea(const std::string& filename)
+{
+
+}
+
+void closeArea()
+{
+
+}
+
+void endBenckmark(const std::string& filename)
+{
+
+}
+
+#endif
