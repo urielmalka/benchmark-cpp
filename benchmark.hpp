0b575d093a29d1b2cbe7385d4cab830994496ddc
urielmalka <urielmalka1@gmail.com>
2025-08-31 12:30:23 +0300

    #

--- benchmark.hpp
+++ benchmark.hpp
@@ -3,18 +3,28 @@
 
 #include <iostream>
 #include <ctime>
+#include <string>
+#include <vector>
 
 clock_t start;
-int task_id = 0;
+int task_id = -1;
+std::vector<std::vector<std::string>> results;
+inline std::string currect_area = "task_id: " + std::to_string(task_id);
 
 void initBenchmark()
 {
     start = clock();
 }
 
-void startArea(const std::string& filename)
+void startArea(const std::string& name_area = "")
 {
-
+    task_id += 1;
+    if(name_area.length() > 0){
+        currect_area = name_area + " (task_id=" + std::to_string(task_id) + ")";
+    }else{
+        currect_area = "task_id=" + std::to_string(task_id);
+    }
+    
 }
 
 void closeArea()
@@ -22,7 +32,7 @@ void closeArea()
 
 }
 
-void endBenckmark(const std::string& filename)
+void endBenckmark(const std::string& file_name)
 {
     /* save to file */
 }
