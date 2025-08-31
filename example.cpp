178737d8d3da098ec16f8f1fdaa8f1589b692e19
urielmalka <urielmalka1@gmail.com>
2025-08-31 12:28:49 +0300

    working on the variables and functions

---
+++ example.cpp
@@ -0,0 +1,13 @@
+#include "benchmark.hpp"
+
+
+int main()
+{
+    std::cout << currect_area << std::endl;
+
+    startArea("function a");
+    
+    std::cout << currect_area << std::endl;
+
+    return 0;
+}
